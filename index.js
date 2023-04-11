f4ade50865ed16f96ed85b9b65916547f1455f91
drake bishop <drakemx85@gmail.com>
2023-04-11 17:31:57 -0500

    sent data using fetch() with POST method. we also caught errors (if any) with .catch()

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// Add your code here
+function submitData(userName, userEmail) {
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+            name: userName,
+            email: userEmail
+        })
+    })
+    .then(response => response.json())
+    .then(data => {
+        const newId = data.id;
+        const userDiv = document.createElement("div");
+        userDiv.textContent = newId;
+        document.body.appendChild(userDiv);
+    })
+    .catch(error => {
+        const errorDiv = document.createElement("div");
+        errorDiv.textContent = error.message;
+        document.body.appendChild(errorDiv);
+    });
+}
